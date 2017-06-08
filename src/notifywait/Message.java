e274ac9d5a6fa4116431603a89d702e82fca65e3
ad <ad@ad-PC>
2017-06-08 10:19:46 +0700

    new commit

---
+++ src/notifywait/Message.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package notifywait;
+
+/**
+ *
+ * @author DuyTuan
+ */
+public class Message {
+    private String msg;
+    
+    public Message(String str){
+        this.msg = str;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+    
+    
+}
